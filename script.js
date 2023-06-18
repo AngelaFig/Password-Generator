0e9a303b0c011ec27f6b9c4b1daffe12ffef7a98
AngelaFig <angelafigueroa596@gmail.com>
2023-06-18 13:10:11 -0400

    created password criteria variables, added password generating function,created if else statements and variable to document user choice, created for loop to select from user chosen criteria and generate password

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 // Assignment Code
+let upperCaseCharacters = ["A", "B", "C", "D", "E"]
+let lowerCaseCharacters = ["a","b", "c", "d", "e"]
+let specialCharacters = ["!", "@", "#", "$","%"]
+let numbers = ["0","1","2","3","4","5","6","7","8","9"]
 var generateBtn = document.querySelector("#generate");
 
 // testing
-// console.log(generateBtn);
+console.log(generateBtn);
 // note to self console error-generate password not defined
+function generatePassword() {
+  let passwordLength = prompt("Enter the Length of Password");
+  if(passwordLength < 8 && passwordLength >= 128) {
+    alert("Please Enter Password Between 8-128 Characters");
+    generatePassword()
+  }
+  let confirmUpperCase = confirm("Do you Want UpperCase Characters? ");
+  let confirmLowerCase = confirm("Do you want LowerCase? ");
+  let confirmSpecialCharacter = confirm("Do you want Special Characters? ");
+  let confirmNumber = confirm("Do you want Numbers? ");
+  // if all confirm false then alert to choose at least one confirm & call generatePassword()
+  let finalArray = []
+  if(confirmUpperCase === true) {
+    finalArray = finalArray.append(upperCaseCharacters)
+  }
+  if(confirmLowerCase === true) {
+    finalArray = finalArray.append(lowerCaseCharacters)
+  }
+  if(confirmSpecialCharacter === true) {
+    finalArray = finalArray.append(specialCharacters)
+  }
+  if(confirmNumber === true) {
+    finalArray = finalArray.append(numbers)
+  }
+
+  let randomPassword = ""
+  for(i = 0; i < passwordLength; i++) {
+    let randomIndex = Math.floor(Math.random() * passwordLength) 
+    let randomCharacter = finalArray[randomIndex]
+    randomPassword = randomPassword + randomCharacter
+  }
+  console.log(randomPassword)
+  return randomPassword
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -13,11 +51,13 @@ function writePassword() {
   passwordText.value = password;
 
 }
-// testing
-writePassword();
 // seems that generatePassword() line 10 not defined
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// not sure how to test the event listener
+// not sure how to test the event listener, would I have to creat fucntion next to click?
+// When click, must receive prompts for password criteria
+// user then selects password criteria they want to use
+// must choose length btw 8 and 128 characters
+// user confirm whether to include lowercase, uppercase, numeric, and or special charcters
