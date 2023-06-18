8d3f3b21207e2d358d23941a028c05bda0cd5f93
AngelaFig <angelafigueroa596@gmail.com>
2023-06-18 11:47:35 -0400

    Recorded series of tests against starter code to id errors

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// testing
+// console.log(generateBtn);
+// note to self console error-generate password not defined
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +13,11 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// testing
+writePassword();
+// seems that generatePassword() line 10 not defined
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// not sure how to test the event listener
